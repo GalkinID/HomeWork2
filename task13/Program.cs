7c0375eb6313b7174b5303b175547f4a169e5de2
Ilya <silvercrow735@gmail.com>
2022-12-03 22:55:01 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿int num = int.Parse(Console.ReadLine());
+if (num / 100 == 0)
+{
+    Console.Write("третьей цифры нет");
+}
+else
+{
+    while (num != 0)
+    {   
+        if (num / 100 < 10)
+        {
+        Console.WriteLine(num % 10);
+        break;
+        }
+        num = num / 10;
+    }
+}
